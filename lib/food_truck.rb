ead80b587fb6e757f3b7c741a8402b8df1dcfc0f
Casey Fazio <98674727+casefaz@users.noreply.github.com>
2022-04-26 09:22:37 -0600

    check stock and test for stock item

--- lib/food_truck.rb
+++ lib/food_truck.rb
@@ -3,6 +3,14 @@ class FoodTruck
 attr_reader :name, :inventory
     def initialize(name)
         @name = name
-        @inventory = {}
+        @inventory = Hash.new(0)
+    end
+    
+    def check_stock(item)
+        if inventory.keys.include?(item)
+            inventory[item]
+       else
+           return 0
+       end
     end
 end
--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -10,4 +10,27 @@ RSpec.describe FoodTruck do
         expect(food_truck.name).to eq("Rocky Mountain Pies")
         expect(food_truck.inventory).to eq({})
     end
+
+    it 'can check inventory stock' do
+        food_truck = FoodTruck.new("Rocky Mountain Pies")
+
+        item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+        item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+
+        food_truck.check_stock(item1)
+
+        expect(food_truck.check_stock(item1)).to eq(0)
+    end
+
+    it 'can stock items' do 
+        food_truck = FoodTruck.new("Rocky Mountain Pies")
+
+        item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+        item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+
+        food_truck.stock(item1, 30)
+        expect(food_truck.inventory).to eq({
+            item1 => 30
+        })
+    end
 end
