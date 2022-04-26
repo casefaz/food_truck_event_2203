f5f6cd10ef8c82492ca63f4bcd0c158a4c2ebf20
Casey Fazio <98674727+casefaz@users.noreply.github.com>
2022-04-26 11:28:55 -0600

    total nested nightmare it3 complete

--- lib/event.rb
+++ lib/event.rb
@@ -17,9 +17,35 @@ attr_reader :name, :food_trucks
     def food_trucks_that_sell(item)
         @food_trucks.find_all { |truck| truck.inventory.include?(item) }
     end
+
+    def inventory_helper
+        @food_trucks.flat_map { |truck| truck.inventory.keys }.uniq
+    end
     
     def sorted_item_list
         @food_trucks.flat_map { |truck| truck.inventory.keys }.sort_by { |item| item.name }.uniq
         # require 'pry'; binding.pry
+    end
+
+    # def overstocked_items #hard stuck, moving onto total inventory and then coming back 
+    #    over_50 = @food_trucks.flat_map { |truck| truck.inventory.find_all { |key, value| value >= 50 }}
+
+    #     require 'pry'; binding.pry
+    # end
+
+    def total_inventory
+        total = {}
+        inventory_helper.each do |item|
+            sum = food_trucks.sum do |truck|
+                if truck.inventory.keys.include?(item)
+                    truck.inventory[item]
+                else
+                    0
+                end
+            end
+        total[item] = {quantity: sum, food_trucks: food_trucks_that_sell(item)}
+        end
+        total
+        # require 'pry';binding.pry
     end
 end
